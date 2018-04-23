3f21a37aa0c693d8aaf18cb8ebc0219a9a395b94
Alexander Powell <aepowell95@gmail.com>
2018-04-23 09:35:41 -0400

    Building board and global variables

---
+++ webApp.js
@@ -0,0 +1,23 @@
+//Global
+
+var config = {
+        blackPlayerName: "Player 1",
+        redPlayerName: "Player 2",
+        startingPlayer: "black", //starting player
+        takenMsg: "This position is already taken. Please make another choice.",
+        drawMsg: "This game is a draw.",
+        playerPrefix: "Current Player is: ",
+        winPrefix: "The winner is: ",
+        countToWin: 4,
+    };
+
+//Two-dimensional array representing our board
+var board = [[0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0]];
+
+// Set the starting player.
+var currentPlayer = config.startingPlayer;
