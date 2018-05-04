c3a2473218cd3aae8b6a2a40d5b07a1060ec422a
Jensen Shurbert <shurbertj@kenyon.edu>
2018-05-04 10:59:19 -0400

    I hope this works

--- chatClient.js
+++ chatClient.js
@@ -3,17 +3,43 @@
 // port (8080) must match  port of client
 var socket = io.connect('http://cslab.kenyon.edu:8080');
 // Watch for incomming messages from server (chatapp.js)
+
+var playerCount = 0;
+
 socket.on('message', function(message) {
-  // A join message: {operation: 'join', name: clientname}
-//   if (message.operation == 'join') {
-//     console.log("Joins:" + message.name);
-//     $('#chatBox').html($('#chatBox').html() + "<font color='red'>User joins: </font>" + message.name + "<br />");
-//   }
+  A join message: {operation: 'join', name: clientname}
+  if (message.operation == 'join') {
+    playerCount += 1;
+    if (playerCount == 1){
+    	    console.log("Joins: Player" + playerCount);
+			//send message
+    }
+     if (playerCount == 2){
+    	    console.log("Joins: Player" + playerCount);
+			//send message
+    }
+    if (playerCount > 2){
+    	    console.log("Too many players");
+			//send message
+    }
+    $('#chatBox').html($('#chatBox').html() + "<font color='red'>User joins: </font>" + message.name + "<br />");
+  }
   // A text message: {operation: 'mess', name: clientname, text: message}
   if (message.operation == 'mess') {
     console.log("Mess:" + message.text);
     $('#chatBox').html($('#chatBox').html() + "<font color='red'>" + message.name + ": </font>" + message.text + "<br />");
   }
+})
+
+//add join button here
+// Action if they push the set name button
+$('#name-btn').click(function() {
+  var name = $('#yourname').val();
+  console.log(name + " joins!");
+  socket.emit('message', {
+    operation: "join",
+    name: name
+  });
 })
 
 // Action if they push the set name button
@@ -32,15 +58,7 @@ $('.board button').click(function() {
 })
 
 
-// Action if they push the set name button
-// $('#name-btn').click(function() {
-//   var name = $('#yourname').val();
-//   console.log(name + " joins!");
-//   socket.emit('message', {
-//     operation: "join",
-//     name: name
-//   });
-// })
+
 
 // Action if they push the send message button
 // $('#send-btn').click(function() {
