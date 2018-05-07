d9f3a63ce7d812909df0e7f95e521275fe664349
Jensen Shurbert <shurbertj@kenyon.edu>
2018-05-07 19:23:08 -0400

    Player join working

--- chatClient.js
+++ chatClient.js
@@ -3,64 +3,63 @@
 // port (8080) must match  port of client
 var socket = io.connect('http://cslab.kenyon.edu:8888');
 // Watch for incomming messages from server (chatapp.js)
+var playerNum = "";
 
-var playerCount = 0;
 
 socket.on('message', function(message) {
-  A join message: {operation: 'join', name: clientname}
-  if (message.operation == 'join') {
-    playerCount += 1;
-    if (playerCount == 1){
-    	    console.log("Joins: Player" + playerCount);
-			//send message
-    }
-     if (playerCount == 2){
-    	    console.log("Joins: Player" + playerCount);
-			//send message
-    }
-    if (playerCount > 2){
-    	    console.log("Too many players");
-			//send message
-    }
-    $('#chatBox').html($('#chatBox').html() + "<font color='red'>User joins: </font>" + message.name + "<br />");
+  //A join message: {operation: 'join', name: clientname}
+  if (message.operation == 'accept') {
+  console.log(JSON.stringify(message));
+	playerNum = message.num;
+	console.log("joined as " + playerNum);
   }
-  // A text message: {operation: 'mess', name: clientname, text: message}
-  if (message.operation == 'mess') {
-    console.log("Mess:" + message.text);
-    $('#chatBox').html($('#chatBox').html() + "<font color='red'>" + message.name + ": </font>" + message.text + "<br />");
+  if (message.operation == 'reject') {
+	console.log("rejected");
   }
+    if (message.operation == 'joined') {
+  console.log(JSON.stringify(message));
+	playerNum = message.num;
+	console.log("Opponent joined as " + playerNum);
+  }
+
 })
 
 //add join button here
 // Action if they push the set name button
 $('#name-btn').click(function() {
-  var name = $('#yourname').val();
-  console.log(name + " joins!");
+  console.log("A player pressed the join button");
   socket.emit('message', {
     operation: "join",
-    name: name
   });
 })
 
 // Action if they push the set name button
 $('.board button').click(function() {
 	var y_pos = $('.board tr').index($(this).closest('tr'));
-<<<<<<< HEAD
     var x_pos = $(this).closest('tr').find('td').index($(this).closest('td'));
-=======
-  var x_pos = $(this).closest('tr').find('td').index($(this).closest('td'));  
->>>>>>> 41466775bf31882b186756c3b376a769daec6b36
+
     //console.log(name + " joins!");
     console.log("Position: " + y_pos + x_pos);
 
   socket.emit('message', {
-    operation: "mess",
+    operation: "move",
     //name: name
     text: message
 
   });
 })
 
+
+
+//change operations to column#
+
+
+//operations : join, move, end
+//playerCount for end and move 
+//move also needs an x and y
+
+//in client
+//when this button gets pushed, send a join and look at the message back, "you were accepted"
 
 
 
@@ -75,5 +74,3 @@ $('.board button').click(function() {
 //     text: message
 //   });
 // })
-
-//change operations to column#
