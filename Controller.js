279eaea5a950d97b97b8f42d29e6537e1dfd3f30
ambrosend <ambrosend@kenyon.edu>
2018-05-09 11:42:28 -0400

    c

--- Controller.js
+++ Controller.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
     config.blackPlayerName = "Player1";
     config.redPlayerName = "Player2";
 
-  //  $('.prefix').text(config.playerPrefix);
-  //  $('#player').addClass(currentPlayer).text(config[currentPlayer + "PlayerName"]);
+   $('.prefix').text(config.playerPrefix);
+  $('#player').addClass(currentPlayer).text(config[currentPlayer + "PlayerName"]);
 
 
 
@@ -17,18 +17,18 @@ $(document).ready(function() {
 //         // Detect the x and y position of the button clicked.
 //         var y_pos = $('.board tr').index($(this).closest('tr'));
 //         var x_pos = $(this).closest('tr').find('td').index($(this).closest('td'));
-// 
+//
 //         // Ensure the piece falls to the bottom of the column.
 //         y_pos = dropToBottom(x_pos, y_pos);
-// 
+//
 //         if (positionIsTaken(x_pos, y_pos)) {
 //             alert(config.takenMsg);
 //             return;
 //         }
-// 
+//
 //         addDiscToBoard(currentPlayer, x_pos, y_pos);
 //         printBoard();
-// 
+//
 //         // Check to see if we have a winner.
 //         if (verticalWin() || horizontalWin() || diagonalWin()) {
 //             // Destroy our click listener to prevent further play.
@@ -36,7 +36,7 @@ $(document).ready(function() {
 //             $('.prefix').text(config.winPrefix);
 //             $('.play-again').show("slow");
 //             return;
-// 
+//
 //         } else if (gameIsDraw()) {
 //             // Destroy our click listener to prevent further play.
 //             $('.board button').unbind('click');
@@ -44,7 +44,7 @@ $(document).ready(function() {
 //             $('.play-again').show("slow");
 //             return;
 //         }
-// 
+//
 //         changePlayer();
 //     });
 
