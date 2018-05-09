7df317a632cae0dfeeaaeb770c029b1d869391e1
Jensen Shurbert <shurbertj@kenyon.edu>
2018-05-09 11:14:03 -0400

    Progress on playerTurn

--- chatClient.js
+++ chatClient.js
@@ -15,14 +15,20 @@ socket.on('message', function(message) {
 	playerNumOpp = message.num;
 	console.log("joined as " + playerNumOpp);
   }
+  
   if (message.operation == 'reject') {
 	console.log("rejected");
   }
+  
     if (message.operation == 'joined') {
   	console.log(JSON.stringify(message));
 	playerNum = message.num;
 	console.log("Opponent joined as " + playerNum);
+	if (playerNum == 2){
+		playerTurn = true;
+	}
   }
+  
   if(message.operation == 'moved'){
   	console.log("This should only work if other player moved");
   	oppX = message.xPos;
@@ -35,31 +41,37 @@ socket.on('message', function(message) {
 
     if (verticalWin() || horizontalWin() || diagonalWin()) {
         // Destroy our click listener to prevent further play.
-        $('.board button').unbind('click');
+        //$('.board button').unbind('click');
+        playerTurn = false;
         $('.prefix').text(config.winPrefix);
         $('.play-again').show("slow");
         return;
 
     } else if (gameIsDraw()) {
         // Destroy our click listener to prevent further play.
-        $('.board button').unbind('click');
+        //$('.board button').unbind('click');
+        playerTurn = false;
         $('.message').text(config.drawMsg);
         $('.play-again').show("slow");
         return;
     }
 
     changePlayer();
-    $('.board button').bind('click');
+    //$('.board button').bind('click');
+    playerTurn = true;
 
 	console.log("XPOS: " + oppX);
 	console.log("PLAYER:"+playerNum+" Positions: " + oppX + oppY);
   }
+  
   if(message.operation == 'move') {
   	console.log("This should work if YOU move");
-  $('.board button').unbind('click');
+  //$('.board button').unbind('click');
+  playerTurn = false;
 
   }
-
+  
+  
 })
 
 //add join button here
@@ -73,6 +85,10 @@ $('#name-btn').click(function() {
 
 // Action if they push the set name button
 $('.board button').click(function() {
+	if (playerTurn == false){
+	return;
+	}
+	
 	var y_pos=0;
 	var x_pos=0;
 	y_pos = $('.board tr').index($(this).closest('tr'));
@@ -80,6 +96,18 @@ $('.board button').click(function() {
 	console.log("type:"+typeof y_pos);
     //console.log(name + " joins!");
     console.log("Position: " + x_pos + y_pos);
+    
+           // Ensure the piece falls to the bottom of the column.
+      y_pos = dropToBottom(x_pos, y_pos);
+
+        if (positionIsTaken(x_pos, y_pos)) {
+            alert(config.takenMsg);
+            return;
+        }
+        
+        if(playerTurn= true){
+        addDiscToBoard(currentPlayer, x_pos, y_pos);
+        printBoard();}
 
   socket.emit('message', {
     operation: "move",
